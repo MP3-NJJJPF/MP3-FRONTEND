49585266ca23b33cd31cd6dc411b4ceeb83ad3a2
Nathalia Ortiz Granada <146957027+Nathal25@users.noreply.github.com>
2025-11-11 10:18:03 -0500

    First Commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export const AboutPage: React.FC = () => {
+return (
+<div>
+<h1>Sobre nosotros</h1>
+<p>Conoce a nuestro equipo </p>
+<br />
+<p> No nos vaya a poner muertos please</p>
+</div>
+);
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const HomePage: React.FC = () => {
+return (
+<div>
+<h1>VIDEO-CONFERENCIAS</h1>
+<p>Conectate con tus amigos :p</p>
+</div>
+);
+};
